620fa41e5b45c0e1965646c8f3b5a2c62d65c829
Karim Nassar <karim@karimnassar.com>
2021-03-21 19:53:03 -0400

    added Apache license header

--- Tests/ChartUITests/ChartUITests.swift
+++ Tests/ChartUITests/ChartUITests.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import ChartUI
 
 final class ChartUITests: XCTestCase {
+
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
